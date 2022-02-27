079850c14da93b25cd768514c3ca70da3f396f2d
Nat <ntaktach@gmail.com>
2022-02-26 21:05:15 -0500

    ez 3

--- guesthouse/easy.sql
+++ guesthouse/easy.sql
@@ -7,4 +7,16 @@ FROM
 WHERE
 	guest_id = 1183
 
-    
+--2. When do they get here? List the arrival time and the first and last names for all guests due to arrive on 2016-11-05, order the output by time of arrival.
+SELECT booking.arrival_time, guest.first_name, guest.last_name
+FROM booking
+JOIN guest
+ON booking.guest_id = guest.id
+WHERE
+	YEAR(booking.booking_date) = '2016'
+	AND MONTH(booking.booking_date) = '11'
+	AND DAY(booking.booking_date) = '05'
+ORDER BY booking.arrival_time;
+
+--3. Look up daily rates. Give the daily rate that should be paid for bookings with ids 5152, 5165, 5154 and 5295. Include booking id, room type, number of occupants and the amount.
+
