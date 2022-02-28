dcf55654d80150082993e0aed35f95bfffedae11
Natalie Taktachev <ntaktach@gmail.com>
2022-02-27 23:34:57 -0500

    med 1

--- guesthouse/medium.sql
+++ guesthouse/medium.sql
@@ -1,2 +1,9 @@
 --6. Ruth Cadbury. Show the total amount payable by guest Ruth Cadbury for her room bookings. You should JOIN to the rate table using room_type_requested and occupants.
 
+SELECT SUM(booking.nights * rate.amount)
+FROM booking
+JOIN rate
+ON booking.occupants = rate.occupancy AND booking.room_type_requested = rate.room_type
+    JOIN guest
+    ON guest.id = booking.guest_id
+WHERE guest.first_name = 'Ruth' AND guest.last_name = 'Cadbury' 
