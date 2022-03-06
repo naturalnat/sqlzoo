be39f83a39bf7117c7c578d4e80c4af9f72c5f0a
Natalie Taktachev <ntaktach@gmail.com>
2022-03-06 14:10:45 -0500

    easy 2

--- adventureworks/easy.sql
+++ adventureworks/easy.sql
@@ -3,3 +3,13 @@
 SELECT FirstName, EmailAddress
 FROM Customer
 WHERE CompanyName = 'Bike World';
+
+-- 2.  Show the CompanyName for all customers with an address in City 'Dallas'. 
+
+SELECT CompanyName
+FROM Customer
+JOIN CustomerAddress
+    ON Customer.CustomerID = CustomerAddress.CustomerID
+  JOIN Address
+    ON CustomerAddress.AddressID = Address.AddressID
+WHERE Address.City = 'Dallas';
