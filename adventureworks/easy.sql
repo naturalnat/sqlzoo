94383be8fb5af9be6e31117b9935b66362958572
Natalie Taktachev <ntaktach@gmail.com>
2022-03-06 21:21:41 -0500

    easy 5

--- adventureworks/easy.sql
+++ adventureworks/easy.sql
@@ -22,10 +22,23 @@ JOIN Product
 ON SalesOrderDetail.ProductID = Product.ProductID
 WHERE Product.ListPrice > 1000;
 
--- 4. 
+-- 4. Give the CompanyName of those customers with orders over $100000. Include the subtotal plus tax plus freight.
 
 SELECT Customer.CompanyName
 FROM SalesOrderHeader
 JOIN Customer
 ON SalesOrderHeader.CustomerID = Customer.CustomerID
 WHERE SalesOrderHeader.SubTotal + SalesOrderHeader.TaxAmt + SalesOrderHeader.Freight > 100000;
+ 
+
+ --5. Find the number of left racing socks ('Racing Socks, L') ordered by CompanyName 'Riding Cycles'
+
+ SELECT SUM(SalesOrderDetail.OrderQty) 
+FROM SalesOrderDetail
+JOIN Product
+ON SalesOrderDetail.ProductID = Product.ProductID
+JOIN SalesOrderHeader
+ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
+JOIN Customer
+ON SalesOrderHeader.CustomerID = Customer.CustomerID
+WHERE Product.Name = 'Racing Socks, L' AND Customer.CompanyName = 'Riding Cycles';
